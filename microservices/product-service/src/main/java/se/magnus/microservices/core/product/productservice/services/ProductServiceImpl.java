3b4ebd0af838e8ea0da02daf5dd9b0866aaa6b51
anteroserrano99 <serranoantero13@gmail.com>
2021-03-14 18:48:08 +0100

    Unit testing added to product microservice

--- microservices/product-service/src/main/java/se/magnus/microservices/core/product/productservice/services/ProductServiceImpl.java
+++ microservices/product-service/src/main/java/se/magnus/microservices/core/product/productservice/services/ProductServiceImpl.java
@@ -2,8 +2,10 @@ package se.magnus.microservices.core.product.productservice.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
 import se.magnus.api.core.product.Product;
 import se.magnus.api.core.product.ProductService;
+import se.magnus.util.exceptions.NotFoundException;
 import se.magnus.util.http.ServiceUtil;
 
 @RestController
@@ -14,6 +16,11 @@ public class ProductServiceImpl implements ProductService {
 
     @Override
     public Product getProduct(int productId) {
+
+        if (productId == 13){
+            throw new NotFoundException("the product with id "+ productId + " has not been found");
+        }
+
 
         Product product = new Product(productId,"nombre",0, serviceUtil.getServiceAddress());
 
--- microservices/product-service/src/test/java/se/magnus/microservices/core/product/productservice/ProductServiceApplicationTests.java
+++ microservices/product-service/src/test/java/se/magnus/microservices/core/product/productservice/ProductServiceApplicationTests.java
@@ -1,13 +1,75 @@
 package se.magnus.microservices.core.product.productservice;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+
 class ProductServiceApplicationTests {
 
+	@LocalServerPort
+	private int port;
+
+	@Autowired
+	private WebTestClient client;
+
+
 	@Test
-	void contextLoads() {
+	void getProductById() {
+
+		int productId = 1;
+
+		client.get()
+				.uri("/product/"+ productId)
+				.accept(APPLICATION_JSON)
+				.exchange()
+				.expectStatus().isOk()
+				.expectHeader().contentType(APPLICATION_JSON)
+				.expectBody()
+				.jsonPath("$.productId").isEqualTo(productId);
 	}
+
+
+
+	@Test
+	void getProductByIdInvalidParameter(){
+		client.get()
+				.uri("product/noInteger")
+				.accept(APPLICATION_JSON)
+				.exchange()
+				.expectStatus().isBadRequest()
+				.expectBody();
+
+
+
+
+	}
+
+	@Test
+	public void getProductNotFound() {
+
+		int productIdNotFound = 13;
+
+		client.get()
+				.uri("/product/" + productIdNotFound)
+				.accept(APPLICATION_JSON)
+				.exchange()
+				.expectStatus().isNotFound()
+				.expectHeader().contentType(APPLICATION_JSON)
+				.expectBody()
+				.jsonPath("$.path").isEqualTo("/product/" + productIdNotFound);
+	}
+
+
+
 
 }
