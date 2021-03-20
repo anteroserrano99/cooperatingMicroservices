1ddde88d5cec8efd22618e4ea277c98d57f1ac35
anteroserrano99 <serranoantero13@gmail.com>
2021-03-20 18:03:35 +0100

    Added swagger documentation, just a test where we can test the swaggerUI path

--- Test/BuildJar.bat
+++ Test/BuildJar.bat
@@ -1,3 +1,3 @@
 cd ../microservices
 
-cd product-composite-service && gradlew bootJar && cd .. && cd product-service && gradlew bootJar && cd .. && cd review-service && gradlew bootJar && cd .. && cd recommendation-service && gradlew bootJar
+cd product-composite-service && gradlew bootJar && cd .. && cd product-service && gradlew bootJar && cd .. && cd review-service && gradlew bootJar && cd .. && cd recommendation-service && gradlew bootJar && cd .. && cd .. && cd api && gradlew bootJar
